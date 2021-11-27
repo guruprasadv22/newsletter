3fc828c7f87340b2b88472a08f51a7b2faf34d09
guruprasadv22 <guruprasadv22@gmail.com>
2021-11-28 01:57:05 +0530

    using bodyparser

--- app.js
+++ app.js
@@ -11,9 +11,12 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-    
+    let firstName = req.body.fname;
+    let lastName = req.body.lname;
+    let email = req.body.userEmail;
+    console.log(firstName, lastName, email);
 });
 
-app.listen(3001, function() {
-    console.log("Server running on port 3001");
+app.listen(3002, function() {
+    console.log("Server running on port 3002");
 })
