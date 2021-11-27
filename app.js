e19c7f2fe70ae877c85ae33d517a79c2863fc9d2
guruprasadv22 <guruprasadv22@gmail.com>
2021-11-27 23:15:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.listen(3000, function() {
+    console.log("Server running on port 3000");
+})
